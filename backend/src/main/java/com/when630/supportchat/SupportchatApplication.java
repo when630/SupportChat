db8e7d441acb546d13d785745ff7c25775f77174
when630 <jel063033@gmail.com>
2025-12-10 15:16:56 +0900

    chore: init project structure

---
+++ backend/src/main/java/com/when630/supportchat/SupportchatApplication.java
@@ -0,0 +1,13 @@
+package com.when630.supportchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/when630/supportchat/SupportchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.when630.supportchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
